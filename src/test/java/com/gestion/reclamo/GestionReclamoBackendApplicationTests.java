7b5d1bbfbd5a1a44d5ac8e16ac8c1f5f6387996a
aawil <aawilliam15@gmail.com>
2024-10-08 12:00:12 -0500

    update test

--- src/test/java/com/gestion/reclamo/GestionReclamoBackendApplicationTests.java
+++ src/test/java/com/gestion/reclamo/GestionReclamoBackendApplicationTests.java
@@ -14,6 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import com.jayway.jsonpath.JsonPath;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 class GestionReclamoBackendAplicationTests {
@@ -25,24 +27,24 @@ class GestionReclamoBackendAplicationTests {
 	    void testListarReclamos() throws Exception {
 	        String token = obtenerToken(); 
 
-	        mockMvc.perform(get("http://web-backend-b9cxcpawaxbreqdu.canadacentral-01.azurewebsites.net/api/v1/reclamos")
+	        mockMvc.perform(get("https://web-backend-b9cxcpawaxbreqdu.canadacentral-01.azurewebsites.net/api/v1/reclamos")
 	                .header("Authorization",  token)) 
 	                .andExpect(status().isOk()) 
 	                .andExpect(jsonPath("$").isArray());
 	    }
 	   
 	   
-	   
 	   private String obtenerToken() throws Exception {
 	        String loginJson = "{\"user\":\"admin\",\"pwd\":\"admintest\"}";
 
-	        MvcResult result = mockMvc.perform(post("http://web-backend-b9cxcpawaxbreqdu.canadacentral-01.azurewebsites.net/api/v1/login")
+	        MvcResult result = mockMvc.perform(post("https://web-backend-b9cxcpawaxbreqdu.canadacentral-01.azurewebsites.net/api/v1/login")
 	                .contentType("application/json")
 	                .content(loginJson))
 	                .andExpect(status().isOk()) 
 	                .andReturn();
 
-	        String token = result.getResponse().getContentAsString();
-	        return token; 
+	        
+	        String token = JsonPath.read(result.getResponse().getContentAsString(), "$.token");
+	        return token;
 	    }
 }
